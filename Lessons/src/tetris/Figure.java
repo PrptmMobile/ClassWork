308fd9bc2d114a012374b3ee5723602f96bbc2f7
Zaal <Zaal_Lyanov@epam.com>
2015-06-08 22:04:41 +0400

    lesson 150608 Tetris

---
+++ Lessons/src/tetris/Figure.java
@@ -0,0 +1,23 @@
+package tetris;
+
+public class Figure {
+	
+	public static final Figure FOUR_HORIZONTAL; 
+	
+	static {
+		FOUR_HORIZONTAL = new Figure();
+		FOUR_HORIZONTAL._data = new int[][] {
+				{0,0,0,0},	
+				{0,0,0,0},	
+				{1,1,1,1},	
+				{0,0,0,0},	
+		};
+	}
+	
+	int[][] _data;
+
+	public Figure() {
+		_data = new int[4][4];
+	}
+
+}
---
+++ Lessons/src/tetris/State.java
@@ -0,0 +1,28 @@
+package tetris;
+
+public class State {
+
+	Figure _figure;
+	Field _field;
+	
+	int row, col;
+
+	public Figure getFigure() {
+		return _figure;
+	}
+
+	public void setFigure(final Figure figure) {
+		_figure = figure;
+		row = 0;
+		col = _field.getWidth()/2 - 2;
+	}
+
+	public Field getField() {
+		return _field;
+	}
+
+	public void setField(final Field field) {
+		_field = field;
+	}
+
+}
